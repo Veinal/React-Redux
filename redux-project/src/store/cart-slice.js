290b5556cc5eccd02aff3559e725ba9c12360641
Veinal <veinalsequeira5229@gmail.com>
2025-01-18 12:09:44 +0530

    third commit

--- redux-project/src/store/cart-slice.js
+++ redux-project/src/store/cart-slice.js
@@ -3,31 +3,43 @@ import { createSlice } from "@reduxjs/toolkit";
 const cartSlice = createSlice({
     name : 'cart',
     initialState : {
-        itemList : [],
+        itemsList : [],
         totalQuantity : 0,
         showCart : false
     },
     reducers: {
         addToCart(state,action) {
             const newItem = action.payload;
-            const existingItem = state.itemList.find((item) => item.id === newItem.id)
+            const existingItem = state.itemsList.find((item) => item.id === newItem.id)
 
             if(existingItem){
                 existingItem.quantity++
-                existingItem.price += newItem.price
+                existingItem.totalPrice += newItem.price
             }else{
-                state.itemList.push({
+                state.itemsList.push({
                     id : newItem.id,
-                    price:newItem.price,
+                    price : newItem.price,
                     quantity : 1,
                     totalPrice : newItem.price,
                     name : newItem.name
                 })
+                state.totalQuantity++
             }
         },
-        removeFromCart() {},
+        removeFromCart(state,action) {
+            const id = action.payload;
+
+            const existingItem = state.itemsList.find(item => item.id === id)
+            if(existingItem.quantity === 1){
+                state.itemsList = state.itemsList.filter(item => item.id !== id)
+                state.totalQuantity--;
+            }else{
+                existingItem.quantity--;
+                existingItem.totalPrice -= existingItem.price
+            }
+        },
         showCart(state) {
-            state.showCart = true
+            state.showCart = !state.showCart
         }
     }
 })
