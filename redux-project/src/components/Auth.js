0621f4d7d08243a57f4d5838bb36ccb3472f5da5
Veinal <veinalsequeira5229@gmail.com>
2025-01-18 10:47:10 +0530

    second commit

---
+++ redux-project/src/components/Auth.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import "./Auth.css";
+import { useDispatch } from "react-redux";
+import authSlice from "../store/auth-slice";
+
+const Auth = () => {
+
+  const dispatch = useDispatch()
+
+  const HandleSubmit = (e) =>{
+    e.preventDefault()
+
+    dispatch(authSlice.actions.login())
+  }
+
+  return (
+    <div className="container">
+      <h1>Login</h1>{" "}
+      <form onSubmit={HandleSubmit}>
+        <label htmlFor="id">Id</label>
+        <input type="text" name="id" id="id" />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+        <button className="login-btn" type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Auth;
